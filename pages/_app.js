e31f1d50d9485b96746fecb176863d885ba93d52
ManuelLG92 <lavacars.ecologic@gmail.com>
2021-08-02 01:46:09 +0200

    First commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
 import '../styles/globals.css'
+import NavBar from "../components/NavBar/NavBar";
+import {Box} from "@material-ui/core";
+import {Image} from "@material-ui/icons";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <>
+
+
+    <NavBar />
+    <Component {...pageProps} />
+  </>
+  )
 }
 
 export default MyApp
